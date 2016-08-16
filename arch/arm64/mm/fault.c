c887702a280ce3c9b4c474b15686a80792cbc08d
Chris Metcalf <cmetcalf@mellanox.com>
2016-08-16 17:19:31 -0400

    arch/arm64: enable task isolation functionality
    In do_notify_resume(), call task_isolation_ready() for
    TIF_TASK_ISOLATION tasks when we are checking the thread-info flags;
    and after we've handled the other work, call task_isolation_enter()
    for such tasks.  To ensure we always call task_isolation_enter() when
    returning to userspace, add _TIF_TASK_ISOLATION to _TIF_WORK_MASK,
    while leaving the old bitmask value as _TIF_WORK_LOOP_MASK to
    check while looping.
    
    We tweak syscall_trace_enter() slightly to carry the "flags"
    value from current_thread_info()->flags for each of the tests,
    rather than doing a volatile read from memory for each one.  This
    avoids a small overhead for each test, and in particular avoids
    that overhead for TIF_NOHZ when TASK_ISOLATION is not enabled.
    
    We instrument the smp_send_reschedule() routine so that it checks for
    isolated tasks and generates a suitable warning if we are about
    to disturb one of them in strict or debug mode.
    
    Finally, report on page faults in task-isolation processes in
    do_page_faults().
    
    This patch is ported to kernel 4.9.x by Alex Belits.
    
    Signed-off-by: Chris Metcalf <cmetcalf@mellanox.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Alex Belits <alex.belits@cavium.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -29,6 +29,7 @@
 #include <linux/sched.h>
 #include <linux/highmem.h>
 #include <linux/perf_event.h>
+#include <linux/isolation.h>
 #include <linux/preempt.h>
 
 #include <asm/bug.h>
@@ -393,8 +394,13 @@ retry:
 	 * Handle the "normal" case first - VM_FAULT_MAJOR
 	 */
 	if (likely(!(fault & (VM_FAULT_ERROR | VM_FAULT_BADMAP |
-			      VM_FAULT_BADACCESS))))
+			      VM_FAULT_BADACCESS)))) {
+		/* No signal was generated, but notify task-isolation tasks. */
+		if (user_mode(regs))
+			task_isolation_quiet_exception("page fault at %#lx",
+						       addr);
 		return 0;
+	}
 
 	/*
 	 * If we are in kernel mode at this point, we have no context to
