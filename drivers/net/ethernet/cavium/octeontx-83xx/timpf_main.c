fbf2e384e2e4539c072528374accd2b3c956450e
Lukasz Bartosik <lb@semihalf.com>
2017-07-31 17:29:11 +0200

    octeontx: fix loading of modules dependencies
    To load dependent modules try_then_request_module was called from
    probe function of a given module which could lead to a deadlock.
    To fix the issue try_then_request_module calls were moved to
    modules init functions.
    
    Signed-off-by: Lukasz Bartosik <lb@semihalf.com>
    Reviewed-by: Stanislaw Kardach <kda@semihalf.com>
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

--- drivers/net/ethernet/cavium/octeontx-83xx/timpf_main.c
+++ drivers/net/ethernet/cavium/octeontx-83xx/timpf_main.c
@@ -618,10 +618,6 @@ static int tim_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	struct timpf *tim;
 	int err = -ENOMEM;
 
-	rst = try_then_request_module(symbol_get(rst_com), "rst");
-	if (!rst)
-		return -ENODEV;
-
 	tim = devm_kzalloc(dev, sizeof(*tim), GFP_KERNEL);
 	if (!tim)
 		return -ENOMEM;
@@ -682,7 +678,6 @@ static void tim_remove(struct pci_dev *pdev)
 	}
 	spin_unlock(&octeontx_tim_dev_lock);
 
-	symbol_put(rst_com);
 	tim_irq_free(tim);
 	tim_sriov_configure(pdev, 0);
 }
@@ -709,6 +704,9 @@ MODULE_DEVICE_TABLE(pci, tim_id_table);
 static int __init tim_init_module(void)
 {
 	pr_info("%s, ver %s\n", DRV_NAME, DRV_VERSION);
+	rst = try_then_request_module(symbol_get(rst_com), "rst");
+	if (!rst)
+		return -ENODEV;
 
 	return pci_register_driver(&tim_driver);
 }
@@ -716,6 +714,7 @@ static int __init tim_init_module(void)
 static void __exit tim_cleanup_module(void)
 {
 	pci_unregister_driver(&tim_driver);
+	symbol_put(rst_com);
 }
 
 module_init(tim_init_module);
