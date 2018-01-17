0dcd58a5944bfe9f4ed8755009c722e85671d8d9
Lukasz Bartosik <lukasz.bartosik@cavium.com>
2018-01-17 09:57:52 +0100

    crypto: cpt - add support for AES-GCM mode to CPT
    Signed-off-by: Lukasz Bartosik <lukasz.bartosik@cavium.com>

--- drivers/crypto/cavium/cpt/cptvf_algs.h
+++ drivers/crypto/cavium/cpt/cptvf_algs.h
@@ -22,6 +22,18 @@
 #define DMA_MODE_FLAG(dma_mode) \
 	(((dma_mode) == DMA_GATHER_SCATTER) ? (1 << 7) : 0)
 
+/* Size of salt in AES GCM mode */
+#define AES_GCM_SALT_SIZE	4
+/* Size of IV in AES GCM mode */
+#define AES_GCM_IV_SIZE		8
+/* Size of ICV (Integrity Check Value) in AES GCM mode */
+#define AES_GCM_ICV_SIZE	16
+/* Offset of IV in AES GCM mode */
+#define AES_GCM_IV_OFFSET	8
+
+/* Truncated SHA1 digest size */
+#define SHA1_TRUNC_DIGEST_SIZE	12
+
 enum req_type {
 	AE_CORE_REQ,
 	SE_CORE_REQ,
@@ -38,7 +50,18 @@ enum cipher_type {
 	AES_XTS = 0x8
 };
 
-enum aes_type {
+enum mac_type {
+	MAC_NULL	= 0x0,
+	MD5		= 0x1,
+	SHA1		= 0x2,
+	SHA224		= 0x3,
+	SHA256		= 0x4,
+	SHA384		= 0x5,
+	SHA512		= 0x6,
+	GMAC		= 0x7
+};
+
+enum aes_key_len {
 	AES_128_BIT = 0x1,
 	AES_192_BIT = 0x2,
 	AES_256_BIT = 0x3
@@ -52,7 +75,7 @@ union encr_ctrl {
 		u64 reserved1:1;
 		u64 aes_key:2;
 		u64 iv_source:1;
-		u64 hash_type:4;
+		u64 mac_type:4;
 		u64 reserved2:3;
 		u64 auth_input_type:1;
 		u64 mac_len:8;
@@ -68,7 +91,7 @@ union encr_ctrl {
 		u64 mac_len:8;
 		u64 auth_input_type:1;
 		u64 reserved2:3;
-		u64 hash_type:4;
+		u64 mac_type:4;
 		u64 iv_source:1;
 		u64 aes_key:2;
 		u64 reserved1:1;
@@ -110,9 +133,26 @@ struct cvm_des3_ctx {
 	u8 des3_key[MAX_KEY_SIZE];
 };
 
+union offset_ctrl_word {
+	u64 flags;
+	struct {
+#if defined(__BIG_ENDIAN_BITFIELD)
+		u64 reserved:32;
+		u64 enc_data_offset:16;
+		u64 iv_offset:8;
+		u64 auth_offset:8;
+#else
+		u64 auth_offset:8;
+		u64 iv_offset:8;
+		u64 enc_data_offset:16;
+		u64 reserved:32;
+#endif
+	} e;
+};
+
 struct cvm_req_ctx {
 	struct cpt_request_info cpt_req;
-	u64 control_word;
+	union offset_ctrl_word ctrl_word;
 	struct fc_context fctx;
 };
 
@@ -121,15 +161,18 @@ struct sdesc {
 };
 
 struct cvm_aead_ctx {
+	u8 key[MAX_KEY_SIZE];
 	struct crypto_shash *hashalg;
 	struct sdesc *sdesc;
-	u32 enckeylen;
-	u32 authkeylen;
-	u32 keylen;
-	u8 key[MAX_KEY_SIZE];
 	u8 *ipad;
 	u8 *opad;
+	u32 enc_key_len;
+	u32 auth_key_len;
+	u8 cipher_type;
+	u8 mac_type;
+	u8 key_type;
 };
 
 int cptvf_do_request(void *cptvf, struct cpt_request_info *req);
+
 #endif /*_CPTVF_ALGS_H_*/
