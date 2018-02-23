b095968fa6df983b7dbb5354ebff3734ef72914d
Abhijit Ayarekar <abhijit.ayarekar@caviumnetworks.com>
2018-02-22 20:56:43 -0800

    net: Add ipfwd offload module support.
    ipfwd offload module sets packet rx/tx callbacks.
    
    Normal packet flow is maintained when the module is not loaded.
    
    Signed-off-by: Abhijit Ayarekar <abhijit.ayarekar@caviumnetworks.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -502,6 +502,126 @@ typedef unsigned int sk_buff_data_t;
 typedef unsigned char *sk_buff_data_t;
 #endif
 
+#if defined(CONFIG_CAVIUM_IPFWD_OFFLOAD)
+
+struct iphdr_new {
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u8    ihl:4,
+			version:4;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	__u8    version:4,
+			ihl:4;
+#else
+#error  "Please fix <asm/byteorder.h>"
+#endif
+	__u8    tos;
+	__be16  tot_len;
+	__be16  id;
+	__be16  frag_off;
+	__u8    ttl;
+	__u8    protocol;
+	__sum16 check;
+	__be32  saddr;
+	__be32  daddr;
+	/*The options start here. */
+};
+
+struct ipv6hdr_new {
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u8                    priority:4,
+							version:4;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	__u8                    version:4,
+							priority:4;
+#else
+#error "Please fix <asm/byteorder.h>"
+#endif
+	__u8                    flow_lbl[3];
+
+	__be16                  payload_len;
+	__u8                    nexthdr;
+	__u8                    hop_limit;
+
+	uint64_t                saddrhi;
+	uint64_t                saddrlo;
+	uint64_t                daddrhi;
+	uint64_t                daddrlo;
+};
+
+struct cvm_udphdr {
+	__be16  source;
+	__be16  dest;
+	__be16  len;
+	__sum16 check;
+};
+
+struct cvm_pppoe_hdr {
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u8 ver : 4;
+	__u8 type : 4;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	__u8 type : 4;
+	__u8 ver : 4;
+#else
+#error  "Please fix <asm/byteorder.h>"
+#endif
+	__u8 code;
+	__be16 sid;
+	__be16 length;
+};
+
+struct cvm_vlan_hdr {
+	__be16 h_vlan_TCI;
+	__be16 h_vlan_encapsulated_proto;
+};
+
+struct cvm_vxlan_hdr {
+	__be32 vx_flags;
+	__be32 vx_vni;
+};
+
+struct gre_hdr_info {
+	__be16 flags;
+	__be16 protocol;
+	__be32 key;
+	__be32 headroom;
+};
+
+
+struct cvm_packet_info {
+	unsigned long           rx_pkt_flags;
+	unsigned long           tx_pkt_flags;
+	u32                     cookie;         /* magic number */
+	void                    *bucket;        /* bucket ptr */
+	void                    *frag_bucket;   /* Frag bucket ptr */
+	u32                     seq;            /* TCP sequence number */
+	u32                     ack_seq;        /* TCP ack seq number */
+	int                     qos_level;
+	struct cvm_vlan_hdr     vlan;
+	struct cvm_vxlan_hdr    vxlan;
+	struct cvm_pppoe_hdr    pppoe;
+	struct gre_hdr_info     gre;
+	union {
+		struct iphdr_new        outer_ip4;      /* IP header */
+		struct ipv6hdr_new      outer_ip6;
+	};
+	struct cvm_udphdr       outer_udp;
+	union {
+		struct {
+			u32                     old_reserved1;
+			struct iphdr_new        cvm_ip; /* IP header */
+			u64                     reserved2[2];
+		} cvm_ip4;
+		struct ipv6hdr_new  cvm_ip6;
+	} cvm_ip4_ip6_u;
+	u64     reserved2[2];   /* L4 ports and reserved */
+#define cvmip                            cvm_ip4_ip6_u.cvm_ip4.cvm_ip
+#define cvmip6                           cvm_ip4_ip6_u.cvm_ip6
+};
+#endif
+
+
+
 /**
  * struct skb_mstamp - multi resolution time stamps
  * @stamp_us: timestamp in us resolution
@@ -791,6 +911,11 @@ struct sk_buff {
 	__u16			transport_header;
 	__u16			network_header;
 	__u16			mac_header;
+
+#if defined(CONFIG_CAVIUM_IPFWD_OFFLOAD)
+	struct cvm_packet_info	cvm_info;
+#endif
+
 
 	/* private: */
 	__u32			headers_end[0];
