89128366af0791fe62d2da288c215276e4efadee
Yury Norov <ynorov@caviumnetworks.com>
2018-03-29 17:25:02 +0300

    smp: Lazy synchronization for EQS CPUs in kick_all_cpus_sync()
    kick_all_cpus_sync() forces all CPUs to sync caches by sending eroadcast
    IPI.  If CPU is in extended quiescent state (idle task or nohz_full
    userspace), this work may be done at the exit of this state. Delaying
    synchronization helps to save power if CPU is in idle state and decrease
    latency for real-time tasks.
    
    This patch introduces rcu_get_eqs_cpus() and uses it in
    kick_all_cpus_sync() to delay syncronization.
    
    For task isolation (https://lkml.org/lkml/2017/11/3/589), IPI to the CPU
    running isolated task is fatal, as it breaks isolation. The approach with
    lazy synchronization helps to maintain isolated state.
    
    I've tested it with test from task isolation series on ThunderX2 for
    more than 10 hours (10k giga-ticks) without breaking isolation.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

--- include/linux/rcutree.h
+++ include/linux/rcutree.h
@@ -74,6 +74,7 @@ static inline void synchronize_rcu_bh_expedited(void)
 void rcu_barrier(void);
 void rcu_barrier_bh(void);
 void rcu_barrier_sched(void);
+void rcu_get_eqs_cpus(struct cpumask *cpus, int choose_eqs);
 unsigned long get_state_synchronize_rcu(void);
 void cond_synchronize_rcu(unsigned long oldstate);
 unsigned long get_state_synchronize_sched(void);
