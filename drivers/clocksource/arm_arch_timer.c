5331393f19e475e6e379f78a76c914f730b7e906
Yury Norov <ynorov@caviumnetworks.com>
2018-05-16 11:18:59 +0300

    arm64: change compat_elf_hwcap and compat_elf_hwcap2 prefix to a32
    ILP32 patch series introduces new type of binaries which is also compat.
    So rename existung aarch32 compat_elf_hwcap's helps to avoid confusing.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>
    Signed-off-by: Yury Norov <ynorov@marvell.com>

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -807,7 +807,7 @@ static void arch_timer_evtstrm_enable(int divider)
 	elf_hwcap |= HWCAP_EVTSTRM;
 #endif
 #ifdef CONFIG_AARCH32_EL0
-	compat_elf_hwcap |= COMPAT_HWCAP_EVTSTRM;
+	a32_elf_hwcap |= COMPAT_HWCAP_EVTSTRM;
 #endif
 	cpumask_set_cpu(smp_processor_id(), &evtstrm_available);
 }
