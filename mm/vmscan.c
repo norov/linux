e6e0d41e3b1beab0c697e35b67bcc74b2cedb393
Yury Norov <yury.norov@gmail.com>
2021-08-07 21:30:05 -0700

    mm: demoton: demote cgroup-limited memory
    Now as user has fine-grained control over demotion, it looks
    unnecessary to restrict demotion in cgroup-limited environment. It's
    possible to disable demotion for task/memory region by (not) passing
    MPOL_F_DEMOTION, if /sys/kernel/mm/numa/demotion_enable is set to
    'mbind', or by disabling demotion for a node associate to the cgroup:
    	echo "-1>node_number" > /sys/kernel/mm/numa/demotion_list
    
    Signed-off-by: Yury Norov <yury.norov@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -541,9 +541,6 @@ static bool can_demote(int nid, struct scan_control *sc)
 	if (sc) {
 		if (sc->no_demotion)
 			return false;
-		/* It is pointless to do demotion in memcg reclaim */
-		if (cgroup_reclaim(sc))
-			return false;
 	}
 	if (next_demotion_node(nid) == NUMA_NO_NODE)
 		return false;
