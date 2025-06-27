bdd89a5367a7c097c30bfbcb791a0548be14b3bf
Yury Norov <yury.norov@gmail.com>
2025-06-27 11:56:28 -0400

    smp: Defer cpumask_ipi cleanup to where it is actually used
    smp_call_function_many_cond() cleans up cfd->cpumask_ipi as soon as
    remote execution requirement detected. On the other hand, the following
    optimization calls send_call_function_single_ipi() if only one remote
    CPU is selected.
    
    Therefore we can initialize ->cpumask_ipi only if nr_cpus > 1.

--- kernel/smp.c
+++ kernel/smp.c
@@ -809,7 +809,6 @@ static void smp_call_function_many_cond(const struct cpumask *mask,
 	cpumask_and(cfd->cpumask, mask, cpu_online_mask);
 	__cpumask_clear_cpu(this_cpu, cfd->cpumask);
 
-	cpumask_clear(cfd->cpumask_ipi);
 	for_each_cpu(cpu, cfd->cpumask) {
 		call_single_data_t *csd = per_cpu_ptr(cfd->csd, cpu);
 
@@ -831,6 +830,11 @@ static void smp_call_function_many_cond(const struct cpumask *mask,
 
 		if (!llist_add(&csd->node.llist, &per_cpu(call_single_queue, cpu)))
 			continue;
+
+		if (nr_cpus == 1) {
+			cpumask_clear(cfd->cpumask_ipi);
+			__cpumask_set_cpu(last_cpu, cfd->cpumask_ipi);
+		}
 
 		__cpumask_set_cpu(cpu, cfd->cpumask_ipi);
 		nr_cpus++;
