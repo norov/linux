c3510b7e18f443c93449a51d65374aa3ea9bd3a1
Yury Norov (NVIDIA) <yury.norov@gmail.com>
2025-09-12 21:17:39 -0400

    crypto: ccp - simplify ccp_find_and_assign_lsb_to_q()
    The function opencodes find_first_and_bit() in a very ineffiecient way.
    Switch to the proper API and drop most of housekeeping code.

--- drivers/crypto/ccp/ccp-dev-v5.c
+++ drivers/crypto/ccp/ccp-dev-v5.c
@@ -611,48 +611,30 @@ static int ccp_find_and_assign_lsb_to_q(struct ccp_device *ccp,
 {
 	DECLARE_BITMAP(qlsb, MAX_LSB_CNT);
 	int bitno;
-	int qlsb_wgt;
 	int i;
 
 	/* For each queue:
 	 * If the count of potential LSBs available to a queue matches the
-	 * ordinal given to us in lsb_cnt:
-	 * Copy the mask of possible LSBs for this queue into "qlsb";
-	 * For each bit in qlsb, see if the corresponding bit in the
-	 * aggregation mask is set; if so, we have a match.
-	 *     If we have a match, clear the bit in the aggregation to
-	 *     mark it as no longer available.
-	 *     If there is no match, clear the bit in qlsb and keep looking.
+	 * ordinal given to us in lsb_cnt, find possible LSB with the
+	 * corresponding bit in the aggregation mask is set.
 	 */
 	for (i = 0; i < ccp->cmd_q_count; i++) {
 		struct ccp_cmd_queue *cmd_q = &ccp->cmd_q[i];
 
-		qlsb_wgt = bitmap_weight(cmd_q->lsbmask, MAX_LSB_CNT);
+		if (bitmap_weight(cmd_q->lsbmask, MAX_LSB_CNT) != lsb_cnt)
+			continue;
 
-		if (qlsb_wgt == lsb_cnt) {
-			bitmap_copy(qlsb, cmd_q->lsbmask, MAX_LSB_CNT);
+		bitno = find_first_and_bit(cmd_q->lsbmask, lsb_pub, MAX_LSB_CNT);
+		if (bitno >= MAX_LSB_CNT)
+			return -EINVAL;
 
-			bitno = find_first_bit(qlsb, MAX_LSB_CNT);
-			while (bitno < MAX_LSB_CNT) {
-				if (test_bit(bitno, lsb_pub)) {
-					/* We found an available LSB
-					 * that this queue can access
-					 */
-					cmd_q->lsb = bitno;
-					bitmap_clear(lsb_pub, bitno, 1);
-					dev_dbg(ccp->dev,
-						 "Queue %d gets LSB %d\n",
-						 i, bitno);
-					break;
-				}
-				bitmap_clear(qlsb, bitno, 1);
-				bitno = find_first_bit(qlsb, MAX_LSB_CNT);
-			}
-			if (bitno >= MAX_LSB_CNT)
-				return -EINVAL;
-			n_lsbs--;
-		}
+		/* We found an available LSB that this queue can access */
+		cmd_q->lsb = bitno;
+		__clear_bit(bitno, lsb_pub);
+		dev_dbg(ccp->dev, "Queue %d gets LSB %d\n", i, bitno);
+		n_lsbs--;
 	}
+
 	return n_lsbs;
 }
 
