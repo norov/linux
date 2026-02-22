7402f5cc9265ac58854a5e28fa0305bda6f459ab
Yury Norov <ynorov@nvidia.com>
2026-02-22 10:33:04 -0500

    bitmap: add test_zero_nbits()

--- lib/test_bitmap.c
+++ lib/test_bitmap.c
@@ -1467,6 +1467,67 @@ static void __init test_bitmap_write_perf(void)
 	pr_info("%s:\t\t%llu\n", __func__, time);
 }
 
+static void __init test_zero_nbits(void)
+{
+	bitmap_copy(NULL, NULL, 0);
+
+	bitmap_to_arr32(NULL, NULL, 0);
+	bitmap_from_arr32(NULL, NULL, 0);
+
+	bitmap_to_arr64(NULL, NULL, 0);
+	bitmap_from_arr64(NULL, NULL, 0);
+
+	bitmap_zero(NULL, 0);
+	bitmap_fill(NULL, 0);
+	bitmap_copy(NULL, NULL, 0);
+	bitmap_copy_clear_tail(NULL, NULL, 0);
+
+	bitmap_and(NULL, NULL, NULL, 0);
+	bitmap_or(NULL, NULL, NULL, 0);
+	bitmap_xor(NULL, NULL, NULL, 0);
+
+	bitmap_weight(NULL, 0);
+	bitmap_weight_and(NULL, NULL, 0);
+	bitmap_weight_andnot(NULL, NULL, 0);
+	bitmap_weighted_or(NULL, NULL, NULL, 0);
+	bitmap_weighted_xor(NULL, NULL, NULL, 0);
+
+	bitmap_complement(NULL, NULL, 0);
+	bitmap_equal(NULL, NULL, 0);
+	bitmap_or_equal(NULL, NULL, NULL, 0);
+	bitmap_subset(NULL, NULL, 0);
+
+	bitmap_empty(NULL, 0);
+	bitmap_full(NULL, 0);
+
+	bitmap_set(NULL, 0, 0);
+	bitmap_clear(NULL, 0, 0);
+
+	bitmap_shift_right(NULL, NULL, 0, 0);
+	bitmap_shift_left(NULL, NULL, 0, 0);
+
+	bitmap_read(NULL, 0, 0);
+	bitmap_write(NULL, 0, 0, 0);
+
+	find_first_bit(NULL, 0);
+	find_first_zero_bit(NULL, 0);
+	find_first_and_bit(NULL, NULL, 0);
+	find_first_andnot_bit(NULL, NULL, 0);
+	find_first_and_and_bit(NULL, NULL, NULL, 0);
+
+	find_next_bit(NULL, 0, 0);
+	find_next_and_bit(NULL, NULL, 0, 0);
+	find_next_andnot_bit(NULL, NULL, 0, 0);
+	find_next_zero_bit(NULL, 0, 0);
+
+	find_nth_bit(NULL, 0, 0);
+	find_nth_and_bit(NULL, NULL, 0, 0);
+
+	find_last_bit(NULL, 0);
+	find_next_clump8(NULL, NULL, 0, 0);
+	find_random_bit(NULL, 0);
+}
+
 #undef TEST_BIT_LEN
 
 static void __init selftest(void)
@@ -1490,6 +1551,7 @@ static void __init selftest(void)
 	test_bitmap_read_perf();
 	test_bitmap_weight();
 	test_bitmap_write_perf();
+	test_zero_nbits();
 
 	test_find_nth_bit();
 	test_for_each_set_bit();
